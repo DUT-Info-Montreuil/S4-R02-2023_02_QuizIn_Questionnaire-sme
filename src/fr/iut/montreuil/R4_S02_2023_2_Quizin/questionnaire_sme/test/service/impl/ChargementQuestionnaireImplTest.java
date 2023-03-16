d75f8237759b9b55464b05018fd345e9011af3ab
AyoubB93 <aybouaziz@iut.univ-paris8.fr>
2023-03-16 16:42:51 +0100

    réindexation + version terminer

--- src/fr/iut/montreuil/R4_S02_2023_2_Quizin/questionnaire_sme/test/service/impl/ChargementQuestionnaireImplTest.java
+++ src/fr/iut/montreuil/R4_S02_2023_2_Quizin/questionnaire_sme/test/service/impl/ChargementQuestionnaireImplTest.java
@@ -2,54 +2,61 @@ package fr.iut.montreuil.R4_S02_2023_2_Quizin.questionnaire_sme.test.service.imp
 
 
 
+import fr.iut.montreuil.R4_S02_2023_2_Quizin.questionnaire_sme.Exception.FichierIncorrectExceptions;
+import fr.iut.montreuil.R4_S02_2023_2_Quizin.questionnaire_sme.Exception.FichierPasTrouveExceptions;
+import fr.iut.montreuil.R4_S02_2023_2_Quizin.questionnaire_sme.Exception.FichierVideExceptions;
 import fr.iut.montreuil.R4_S02_2023_2_Quizin.questionnaire_sme.entities.dto.QuestionDTO;
 import fr.iut.montreuil.R4_S02_2023_2_Quizin.questionnaire_sme.entities.dto.QuestionnaireDTO;
 import fr.iut.montreuil.R4_S02_2023_2_Quizin.questionnaire_sme.modeles.IServiceQuestion;
+import fr.iut.montreuil.R4_S02_2023_2_Quizin.questionnaire_sme.test.service.Mock.ChargementQuestionnaireMockIncorrect;
 import fr.iut.montreuil.R4_S02_2023_2_Quizin.questionnaire_sme.test.service.Mock.ChargementQuestionnaireMockOk;
+import fr.iut.montreuil.R4_S02_2023_2_Quizin.questionnaire_sme.test.service.Mock.ChargementQuestionnaireVide;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChargementQuestionnaireImplTest {
-    private IServiceQuestionnaire serviceQuestionnaireTest;
+    private IServiceQuestion serviceQuestionnaireTest;
     @Test
     public void chargementQuestionnaireCorrect() throws FichierPasTrouveExceptions, FichierVideExceptions, FichierIncorrectExceptions {
-        serviceQuestionnaireTest = new ServiceQuestionnaireMockCorrect();
-        QuestionDTO questionCorrect = new QuestionDTO("De quel petit objet se munit le golfeur pour surelever sa balle avant de la frapper ","Tee");
+        serviceQuestionnaireTest = new ChargementQuestionnaireMockOk();
+        QuestionDTO questionCorrect = new QuestionDTO(1,1,"fr","De quel petit objet se munit le golfeur pour surélever sa balle avant de la frapper ?","Tee",1,"Le joueur devant taper sa balle sur le départ peut la poser sur une cheville de bois ou de plastique appelée tee, qui ne peut pas être utilisée en dehors des départs. L'utilité d'un tee est de surélever la balle pour faciliter l'utilisation de clubs de golf spéciaux","https://fr.wikipedia.org/wiki/Mat%C3%A9riel_de_golf"
+        );
         List<QuestionDTO> listQuestionsCorrect = new ArrayList<QuestionDTO>();
         listQuestionsCorrect.add(questionCorrect);
-        QuestionnaireDTO leQuestionnaireCorrect = new QuestionnaireDTO(listQuestionsCorrect);
+        QuestionnaireDTO leQuestionnaireCorrect = new QuestionnaireDTO((ArrayList<QuestionDTO>) listQuestionsCorrect);
         List<QuestionnaireDTO> reponseCorrect = new ArrayList<QuestionnaireDTO>();
         reponseCorrect.add(leQuestionnaireCorrect);
-        List<QuestionnaireDTO> bonnereponse = serviceQuestionnaireTest.chargerListeQuestionnaire("chargementCorrect.csv");
-        Assertions.assertEquals(leQuestionnaireCorrect.getListeQuestions().get(0).getReponse(),bonnereponse.get(0).getListeQuestions().get(0).getReponse());
-        Assertions.assertEquals(leQuestionnaireCorrect.getListeQuestions().get(0).getLibelle(),bonnereponse.get(0).getListeQuestions().get(0).getLibelle());
+        List<QuestionnaireDTO> bonnereponse = serviceQuestionnaireTest.chargerListeQuestion("chargementCorrect.csv");
+        Assertions.assertEquals(leQuestionnaireCorrect.getQuestions().get(0).getReponse(),bonnereponse.get(0).getQuestions().get(0).getReponse());
+        Assertions.assertEquals(leQuestionnaireCorrect.getQuestions().get(0).getLibelle(),bonnereponse.get(0).getQuestions().get(0).getLibelle());
 
     }
 
     @Test
     public void chargementQuestionnaireIncorrect() throws FichierIncorrectExceptions, FichierPasTrouveExceptions, FichierVideExceptions {
-        serviceQuestionnaireTest = new ServiceQuestionnaireMockIncorrect();
+        serviceQuestionnaireTest = new ChargementQuestionnaireMockIncorrect();
         // QuestionDTO questionIncorrectAttendu = new QuestionDTO("De quel petit objet se munit le golfeur pour surelever sa balle avant de la frapper ",null);
         //serviceQuestionnaireTest.chargerListeQuestionnaire("chargementIncorrect.csv");
         Assertions.assertThrows(FichierIncorrectExceptions.class, ()->
-                serviceQuestionnaireTest.chargerListeQuestionnaire("chargementIncorrect.csv"),"Valeur incorrect ou manquante");
+                serviceQuestionnaireTest.chargerListeQuestion("chargementIncorrect.csv"),"Valeur incorrect ou manquante");
 
     }
 
     @Test
     public void chargementQuestionnaireVide() throws FichierIncorrectExceptions, FichierPasTrouveExceptions, FichierVideExceptions {
-        serviceQuestionnaireTest = new ServiceQuestionnaireMockVide();
+        serviceQuestionnaireTest = new ChargementQuestionnaireVide();
         Assertions.assertThrows(FichierVideExceptions.class, ()->
-                serviceQuestionnaireTest.chargerListeQuestionnaire("chargementVide.csv"),"Nom du fichier incorrect ou inexistant");
+                serviceQuestionnaireTest.chargerListeQuestion("chargementVide.csv"),"Nom du fichier incorrect ou inexistant");
     }
 
     @Test
     public void chargementQuestionnaireNomInvalide() throws FichierIncorrectExceptions, FichierPasTrouveExceptions, FichierVideExceptions {
-        serviceQuestionnaireTest = new ServiceQuestionnaireMockNomInvalide();
+        serviceQuestionnaireTest = new ChargementQuestionnaireMockIncorrect();
         Assertions.assertThrows(FichierPasTrouveExceptions.class, ()->
-                serviceQuestionnaireTest.chargerListeQuestionnaire("okok.csv"),"Nom du fichier incorrect ou inexistant");
+                serviceQuestionnaireTest.chargerListeQuestion("okok.csv"),"Nom du fichier incorrect ou inexistant");
     }
 }
 
